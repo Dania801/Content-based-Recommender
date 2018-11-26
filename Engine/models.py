aefc148c1c6c79a499de1a4bf9ae83af25eb94f1
Dania801 <ssansnnom07@gmail.com>
2018-11-26 15:36:52 +0200

    Logistic and naive bayes models

--- Engine/models.py
+++ Engine/models.py
@@ -7,7 +7,8 @@ from collections import Counter
 import pickle
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import OrdinalEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
 from extractor import addExtractedFeatures
 from sklearn import metrics
 
@@ -32,4 +33,40 @@ def LinearRegressionModel():
     print('------------------------------------------------------------')
 
 
-LinearRegressionModel()
+def LogisticRegressionModel():
+    data = addExtractedFeatures()
+    trainData = data [0:int(len(data)/2)]
+    testData = data[int(len(data)/2):]
+    columns = ['Relevant', 'MouseMs', 'Readability', 'Novelty', 'PageMs', 'Authority', 'ConditionalFreq', 'MeanConditionalFreq']
+    xTrainData = trainData[columns]
+    yTrainData = trainData['UserLike']
+    xTestData = testData[columns]
+    yTestData = testData['UserLike']
+    lm = LogisticRegression()
+    lm.fit(xTrainData, yTrainData)
+    predictions = lm.predict(xTestData)
+    predictions = np.array(predictions).astype(int)
+    yTestData = yTestData.values
+    accuracy = lm.score(xTestData, yTestData)
+    print('------------------------------------------------------------')
+    print('Linear Regression model accuracy = {0}%'.format(accuracy*100))
+    print('------------------------------------------------------------')
+
+def NaiveBayesModel():
+    data = addExtractedFeatures()
+    trainData = data [0:int(len(data)/2)]
+    testData = data[int(len(data)/2):]
+    columns = ['Relevant', 'MouseMs', 'Readability', 'Novelty', 'PageMs', 'Authority', 'ConditionalFreq', 'MeanConditionalFreq']
+    xTrainData = trainData[columns]
+    yTrainData = trainData['UserLike']
+    xTestData = testData[columns]
+    yTestData = testData['UserLike']
+    lm = GaussianNB()
+    lm.fit(xTrainData, yTrainData)
+    predictions = lm.predict(xTestData)
+    predictions = np.array(predictions).astype(int)
+    yTestData = yTestData.values
+    accuracy = lm.score(xTestData, yTestData)
+    print('------------------------------------------------------------')
+    print('Linear Regression model accuracy = {0}%'.format(accuracy*100))
+    print('------------------------------------------------------------')
